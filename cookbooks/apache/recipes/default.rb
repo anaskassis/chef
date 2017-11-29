e845a481bc756c1ea587666243aaef49f63be5bb
anas <dranas2010@hotmail.com>
2017-11-29 18:06:40 +0000

    added basic apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,14 +4,8 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved
 #
-if node['platform_family'] == "rhel"
-	package = 'httpd'
-elsif node['plastform_family'] == 'amazon'
-	package = 'httpd'
-end
-
 package 'apache2' do
-	package_name package
+	package_name 'httpd'
 	action :install
 end
 
@@ -19,3 +13,4 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
