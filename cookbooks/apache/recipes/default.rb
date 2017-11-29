0c25b514295bce0d32cc89f46986ab657227b495
anas <dranas2010@hotmail.com>
2017-11-29 17:22:08 +0000

    added motd and changed apache default for node attribute

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved
+#
+if node['platform_family'] == "rhel"
+	package = 'httpd'
+elsif node['plastform_family'] == 'amazon'
+	package = 'httpd'
+end
+
+package 'apache2' do
+	package_name package
+	action :install
+end
+
+service 'apache2' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
